038eb1e73f2df23199f4eddb5d84687464d36cf3
pedmcor <pedmcor@gmail.com>
2018-03-09 09:15:48 -0300

    Change the method for read csv

--- RAEM/raem/lib/raem/fields_mapped.ex
+++ RAEM/raem/lib/raem/fields_mapped.ex
@@ -6,88 +6,110 @@ defmodule Raem.FieldMapped do
   @mappings %{
     ano: %{
       field: "Ano",
-      type: :integer
+      type: :integer,
+      value: "",
     },
     cod_ies: %{
       field: "Código da IES",
-      type: :integer
+      type: :integer,
+      value: "",
     },
     nome_ies: %{
       field: "Nome da IES",
-      type: :string
+      type: :string,
+      value: "",
     },
     sigla_ies: %{
       field: "Sigla da IES",
-      type: :string
+      type: :string,
+      value: "",
     },
     org_academica: %{
       field: "Organização Acadêmica",
-      type: :string
+      type: :string,
+      value: "",
     },
     cat_administrativa: %{
       field: "Categoria Administrativa",
-      type: :string
+      type: :string,
+      value: "",
     },
     cod_curso: %{
       field: "Código do Curso",
-      type: :integer
+      type: :integer,
+      value: "",
     },
     cod_area: %{
       field: "Código da Área",
-      type: :integer
+      type: :integer,
+      value: "",
     },
     area_enquadrament: %{
       field: "Área de Enquadramento",
-      type: :string
+      type: :string,
+      value: "",
     },
     modalidade_ensino: %{
       field: "Modalidade de Ensino",
-      type: :string
+      type: :string,
+      value: "",
     },
     cod_municipio: %{
       field: "Código do Município",
-      type: :integer
+      type: :integer,
+      value: "",
     },
     municipio_curso: %{
       field: "Município do Curso",
-      type: :string
+      type: :string,
+      value: "",
     },
     sigla_uf: %{
       field: "Sigla da UF",
-      type: :string
+      type: :string,
+      value: "",
     },
     concluintes_inscritos: %{
       field: "Concluintes Inscritos",
-      type: :integer
+      type: :integer,
+      value: "",
     },
     concluintes_participantes: %{
       field: "Concluintes Participantes",
-      type: :integer
+      type: :integer,
+      value: "",
     },
     concluintes_participantes_enem: %{
       field: "Concluintes Participantes com nota no Enem",
-      type: :integer
+      type: :integer,
+      value: "",
     },
     percentual_concluintes_participantes_enem: %{
       field: "Percentual de Concluintes participantes com nota no Enem",
-      type: :float
+      type: :float,
+      value: "",
     },
     nota_bruta_idd: %{
       field: "Nota Bruta - IDD",
-      type: :float
+      type: :float,
+      value: "",
     },
     nota_padronizada_idd: %{
       field: "Nota Padronizada - IDD",
-      type: :float
+      type: :float,
+      value: "",
     },
     idd_faixa: %{
       field: "IDD (Faixa)",
-      type: :string
+      type: :string,
+      value: "",
     }
   }
 
   def read_list_field(data) do
-    read_field(data)
+    data
+    |> read_field()
+    #|> create_struct()
   end
 
   def read_field(data) do
@@ -97,10 +119,43 @@ defmodule Raem.FieldMapped do
         @mappings
         |> Enum.map(fn(mapped) ->
           if(elem(mapped, 1).field == elem(map_data,0)) do
-            Map.put(%{}, elem(mapped, 0), elem(map_data,1))
+            Map.put(%{},elem(mapped, 0), elem(map_data,1))
           end
         end)
         |> Enum.filter(& !is_nil(&1))
+    end)
+    |> List.flatten
+  end
+
+  defp create_struct(list_mapped) do
+    %{
+        id: get_field(list, list_mapped),
+        ano: get_field(list),
+        area_enquadramento: get_field(list),
+        cat_administrativa: get_field(list),
+        cod_area: get_field(list),
+        cod_curso: get_field(list),
+        cod_ies: get_field(list),
+        cod_municipio: get_field(list),
+        concluintes_inscritos: get_field(list),
+        concluintes_participantes: get_field(list),
+        concluintes_participantes_enem: get_field(list),
+        idd_faixa: get_field(list),
+        modalidade_ensino: get_field(list),
+        municipio_curso: get_field(list),
+        nome_ies: get_field(list),
+        nota_bruta_idd: get_field(list),
+        nota_padronizada_idd: get_field(list),
+        org_academica: get_field(list),
+        percentual_concluintes_participantes_enem: get_field(list),
+        sigla_ies: get_field(list),
+        sigla_uf: get_field(list),
+        inserted_at: get_field(list),
+        updated_at: get_field(list)
+    }
+    list_mapped
+    |> Enum.map(fn(mapped) ->
+      IO.inspect(mapped)
     end)
   end
 end
