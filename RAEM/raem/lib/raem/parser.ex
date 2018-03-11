820268529f6baa408255f45ff0fb319eb010f3c4
pedmcor <pedmcor@gmail.com>
2018-03-10 21:38:13 -0300

    Update parser

--- RAEM/raem/lib/raem/parser.ex
+++ RAEM/raem/lib/raem/parser.ex
@@ -1,35 +1,21 @@
 defmodule Raem.Parser do
-  alias Raem.Mapper
   use Ecto.Schema
+
+  NimbleCSV.define(MyParser, separator: ",", escape: "\"")
 
   def read_files(file_path) do
     file_path
     |> Enum.map(fn(file) ->
       file
       |> File.stream!
-      |> Stream.map(&(&1))
-      |> CSV.decode!(header: true)
-      #|> Enum.to_list()
-      |> Enum.take(2)
-      |> zip_header_and_map()
-    end)
+      |> MyParser.parse_stream
+      |> Stream.map(&map_csv(&1))
+      |> Enum.to_list
+   end)
   end
 
-  defp zip_header_and_map(list_csv) do
-    header_csv = List.first(list_csv)
-    list_csv
-    |> List.delete_at(0)
-    |> Enum.map(fn(line_csv) ->
-      Enum.zip(header_csv, line_csv)
-    end)
-    |> transform_to_fiel_mapped()
-  end
-
-  defp transform_to_fiel_mapped(list_zipped) do
-    list_zipped
-    |> Enum.map(fn(map) ->
-      map
-      |> Mapper.read_mapped_list()
-    end)
+  defp map_csv ([ano, cod_ies, nome_ies, sigla_ies, org_academica, cat_administrativa, cod_curso, cod_area, area_enquadramento, modalidade_ensino, cod_municipio, municipio_curso, sigla_uf, concluintes_inscritos, concluintes_participantes, concluintes_participantes_enem, percentual_concluintes_participantes_enem, nota_bruta_idd, nota_padronizada_idd, idd_faixa]
+  ) do
+    %{ano: ano, area_enquadramento: area_enquadramento, cat_administrativa: cat_administrativa, cod_area: cod_area, cod_curso: cod_curso, cod_ies: cod_ies, cod_municipio: cod_municipio, concluintes_inscritos: concluintes_inscritos, concluintes_participantes: concluintes_participantes, concluintes_participantes_enem: concluintes_participantes_enem, idd_faixa: idd_faixa, modalidade_ensino: modalidade_ensino, municipio_curso: municipio_curso, nome_ies: nome_ies, nota_bruta_idd: nota_bruta_idd, nota_padronizada_idd: nota_padronizada_idd, org_academica: org_academica, percentual_concluintes_participantes_enem: percentual_concluintes_participantes_enem, sigla_ies: sigla_ies, sigla_uf: sigla_uf}
   end
 end
--- RAEM/raem/mix.exs
+++ RAEM/raem/mix.exs
@@ -41,7 +41,8 @@ defmodule Raem.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:csv, "~> 2.0.0"}
+      {:csv, "~> 2.0.0"},
+      {:nimble_csv, "~> 0.3"}
     ]
   end
 
