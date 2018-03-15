e4aba3368805f9a2f2460cc2ab1ef937a9385354
pedmcor <pedmcor@gmail.com>
2018-03-14 21:06:20 -0300

    Add the parser

--- RAEM/raem/lib/mix/tasks/seed.ex
+++ RAEM/raem/lib/mix/tasks/seed.ex
@@ -1,25 +1,57 @@
 defmodule Mix.Tasks.Seed do
-  @shortdoc "Seeds the dataset"
-
+  @shortdoc """
+  Mix for seed the dataset
+  Receives a path of resources
+  """
   use Mix.Task
   alias Raem.Parser
+  #TODO this is alias for the struct
+  #In future change the alias
   alias Raem.Idds.Idd
+  alias Raem.Igcs.Igc
+  alias Raem.Enades.Enade
+  alias Raem.Cpcs.Cpc
 
-  import Ecto
+  #import Ecto
 
-  @doc false
+  @doc """
+  Run the seed
+  And insert in the database
+  By default get path from resources
+
+  ## Examples
+
+  iex> mix.seed
+
+  """
+
+
   def run(_args) do
-    "resources/IDD/*.{csv}"
-    |> Path.wildcard
-    |> Parser.read_files
-    |> Enum.map(fn(list_readed) ->
-      list_readed
-      |> Enum.map(fn(file_readed) ->
-        file_readed
-        #|> Idd.create_idd
-      end)
-    end)
+    #TODO change the default dir_name
+    #This above is set to run tests
 
-    IO.inspect(%Idd{}.__struct__.__schema__(:fields))
+    default_dir_name = "resources/"
+    path = default_dir_name <> "IDD/*.{csv}"
+
+    struct =
+      path
+      |> Parser.set_struct(default_dir_name)
+      |> case do
+        :IDD ->
+          Idd.create_idd()
+        :IGC ->
+          IGC.create_igc()
+        :ENADE ->
+          ENADE.create_enade()
+        :CPC ->
+          CPC.create_cpc()
+      end
+
+    path
+    |> Path.wildcard
+    |> Parser.parse(:stream)
+    |> Enum.map(fn(row) ->
+      row
+    end)
   end
 end
--- RAEM/raem/lib/raem/parser.ex
+++ RAEM/raem/lib/raem/parser.ex
@@ -1,21 +1,44 @@
 defmodule Raem.Parser do
   use Ecto.Schema
 
-  NimbleCSV.define(MyParser, separator: ",", escape: "\"")
+  alias NimbleCSV.RFC4180, as: CSV
 
-  def read_files(file_path) do
-    file_path
-    |> Enum.map(fn(file) ->
-      file
-      |> File.stream!
-      |> MyParser.parse_stream
-      |> Stream.map(&map_csv(&1))
-      |> Enum.to_list
-   end)
+  def parse(path, list_or_stream \\ :list)
+
+  def parse(path, :stream) do
+    header = path |> header() |> List.to_tuple()
+
+    path
+    |> File.stream!()
+    |> CSV.parse_stream()
+    |> Stream.map(fn row_values ->
+      row_values
+      |> Stream.with_index()
+      |> Stream.map(fn {v, i} -> {elem(header, i), v} end)
+      |> Enum.to_list()
+      |> Map.new()
+    end)
   end
 
-  defp map_csv ([ano, cod_ies, nome_ies, sigla_ies, org_academica, cat_administrativa, cod_curso, cod_area, area_enquadramento, modalidade_ensino, cod_municipio, municipio_curso, sigla_uf, concluintes_inscritos, concluintes_participantes, concluintes_participantes_enem, percentual_concluintes_participantes_enem, nota_bruta_idd, nota_padronizada_idd, idd_faixa]
-  ) do
-    %{ano: ano, area_enquadramento: area_enquadramento, cat_administrativa: cat_administrativa, cod_area: cod_area, cod_curso: cod_curso, cod_ies: cod_ies, cod_municipio: cod_municipio, concluintes_inscritos: concluintes_inscritos, concluintes_participantes: concluintes_participantes, concluintes_participantes_enem: concluintes_participantes_enem, idd_faixa: idd_faixa, modalidade_ensino: modalidade_ensino, municipio_curso: municipio_curso, nome_ies: nome_ies, nota_bruta_idd: nota_bruta_idd, nota_padronizada_idd: nota_padronizada_idd, org_academica: org_academica, percentual_concluintes_participantes_enem: percentual_concluintes_participantes_enem, sigla_ies: sigla_ies, sigla_uf: sigla_uf}
+  def parse(path, :list) do
+    path
+    |> parse(:stream)
+    |> Enum.to_list()
+  end
+
+  defp header(path) do
+    path
+    |> File.stream!()
+    |> CSV.parse_stream(headers: false)
+    |> Stream.take(1)
+    |> Enum.to_list()
+    |> List.first()
+  end
+
+  def set_struct(path, default_dir \\ "resources/") do
+    path
+    |> Path.dirname
+    |> String.trim_leading(default_dir)
+    |> String.to_atom()
   end
 end
