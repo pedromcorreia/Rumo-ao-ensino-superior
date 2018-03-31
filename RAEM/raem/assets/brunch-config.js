36181843956763ffbc71095ec557f77d2388c22b
pedmcor <pedmcor@gmail.com>
2018-03-31 15:39:01 -0300

    Add Boostrap

--- RAEM/raem/assets/brunch-config.js
+++ RAEM/raem/assets/brunch-config.js
@@ -4,23 +4,26 @@ exports.config = {
     javascripts: {
       joinTo: "js/app.js"
 
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
+        // To use a separate vendor.js bundle, specify two files path
+        // http://brunch.io/docs/config#-files-
+        // joinTo: {
+        //  "js/app.js": /^(web\/static\/js)/,
+        //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
+        // }
+        //
+        // To change the order of concatenation of files, explicitly mention here
+        // order: {
+        //   before: [
+        //     "web/static/vendor/js/jquery-2.1.1.js",
+        //     "web/static/vendor/js/bootstrap.min.js"
+        //   ]
+        // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: "css/app.css",
+      order: {
+        after: ["web/static/css/app.css"] // concat app.css last
+      }
     },
     templates: {
       joinTo: "js/app.js"
@@ -29,34 +32,49 @@ exports.config = {
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/assets/static". Files in this directory
+    // By default, we set this to "/web/static/assets". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(static)/
+    assets: /^(web\/static\/assets)/
   },
 
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: [
+      "web/static",
+      "test/static"
+    ],
+
     // Where to compile files to
-    public: "../priv/static"
+    public: "priv/static"
   },
 
   // Configure your plugins
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/vendor/]
+      ignore: [/web\/static\/vendor/]
+    },
+    sass: {
+      options: {
+        includePaths: ["node_modules/bootstrap/scss"], // Tell sass-brunch where to look for files to @import
+        precision: 8 // Minimum precision required by bootstrap-sass
+      }
     }
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["js/app"]
+      "js/app.js": ["web/static/js/app"]
     }
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    globals: { // Bootstrap's JavaScript requires both '$' and 'jQuery' in global scope
+      $: 'jquery',
+      jQuery: 'jquery',
+      bootstrap: 'bootstrap' // Require Bootstrap's JavaScript globally
+    }
   }
 };
